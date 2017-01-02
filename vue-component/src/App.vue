66ffd724460de9736d350a81d5e67f048c42d490
zhengweibo <mgsers@hotmail.com>
2017-01-03 00:19:34 +0800

    add

---
+++ vue-component/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <app-select></app-select>
+  </div>
+</template>
+
+<script>
+import appSelect from './components/select'
+
+export default {
+  name: 'app',
+  components: {
+    appSelect
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-component/src/components/select.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="select-box" :class="{active: isShow}" @click="selectShow()">aa</div>
+</template>
+
+<script>
+export default {
+  name: 'select',
+  data () {
+    return {
+      isShow: false
+    }
+  },
+  methods: {
+    selectShow (){
+      if (this.isShow == false) {
+        this.isShow = true
+      }else {
+        this.isShow = false
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.select-box {
+  margin: 0 auto;
+  width: 150px;
+  height: 30px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  border-radius: 4px;
+}
+.aa {
+  color: red;
+}
+.select-box.active {
+  border: 1px solid #108ee9;
+}
+.select-box:hover {
+  border: 1px solid #108ee9;
+}
+</style>
