c9e089ce016bad9861226d8f42484157efd167d0
zhengweibo <mgsers@hotmail.com>
2017-01-09 18:02:55 +0800

    a lot of change

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,38 @@
     <app-select
     :select-data="source"
     @selectedData='selectedE'>
-  </app-select>
+    </app-select>
+    <tag-input
+    :ipt-data="source"
+    @iptData='iptE'
+    >
+    </tag-input>
   </div>
 </template>
 
 <script>
 import appSelect from './components/select'
+import tagInput from './components/tagInput'
 
 export default {
   name: 'app',
   components: {
-    appSelect
+    appSelect,
+    tagInput
   },
   data () {
     return {
-      source: ['ww','ee','rr','tt']
+      source: [{name: 'aa',checked: false,allow: true},
+               {name: 'bb',checked: true,allow: true},
+               {name: 'cc',checked: false,allow: false},
+               {name: 'dd',checked: false,allow: true}]
     }
   },
   methods: {
     selectedE (v) {
+      console.log(v);
+    },
+    iptE (v) {
       console.log(v);
     }
   }
